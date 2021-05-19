0ef5ed21b75577742b035d5f487427b74764b043
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-20 01:22:02 +0200

    Uncomment and minor format adjustments

--- ps4.py
+++ ps4.py
@@ -1,121 +1,122 @@
 import numpy as np
-from numpy import array
-import pylab
+import pylab as plt
 import re
 
 import os
+
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
 
+# cities in our weather data
+CITIES = [
+    'BOSTON',
+    'SEATTLE',
+    'SAN DIEGO',
+    'PHILADELPHIA',
+    'PHOENIX',
+    'LAS VEGAS',
+    'CHARLOTTE',
+    'DALLAS',
+    'BALTIMORE',
+    'SAN JUAN',
+    'LOS ANGELES',
+    'MIAMI',
+    'NEW ORLEANS',
+    'ALBUQUERQUE',
+    'PORTLAND',
+    'SAN FRANCISCO',
+    'TAMPA',
+    'NEW YORK',
+    'DETROIT',
+    'ST LOUIS',
+    'CHICAGO'
+]
 
-# # cities in our weather data
-# CITIES = [
-#     'BOSTON',
-#     'SEATTLE',
-#     'SAN DIEGO',
-#     'PHILADELPHIA',
-#     'PHOENIX',
-#     'LAS VEGAS',
-#     'CHARLOTTE',
-#     'DALLAS',
-#     'BALTIMORE',
-#     'SAN JUAN',
-#     'LOS ANGELES',
-#     'MIAMI',
-#     'NEW ORLEANS',
-#     'ALBUQUERQUE',
-#     'PORTLAND',
-#     'SAN FRANCISCO',
-#     'TAMPA',
-#     'NEW YORK',
-#     'DETROIT',
-#     'ST LOUIS',
-#     'CHICAGO'
-# ]
-#
-# INTERVAL_1 = list(range(1961, 2006))
-# INTERVAL_2 = list(range(2006, 2016))
-#
-# """
-# Begin helper code
-# """
-# class Climate(object):
-#     """
-#     The collection of temperature records loaded from given csv file
-#     """
-#     def __init__(self, filename):
-#         """
-#         Initialize a Climate instance, which stores the temperature records
-#         loaded from a given csv file specified by filename.
-#
-#         Args:
-#             filename: name of the csv file (str)
-#         """
-#         self.rawdata = {}
-#
-#         f = open(filename, 'r')
-#         header = f.readline().strip().split(',')
-#         for line in f:
-#             items = line.strip().split(',')
-#
-#             date = re.match('(\d\d\d\d)(\d\d)(\d\d)', items[header.index('DATE')])
-#             year = int(date.group(1))
-#             month = int(date.group(2))
-#             day = int(date.group(3))
-#
-#             city = items[header.index('CITY')]
-#             temperature = float(items[header.index('TEMP')])
-#             if city not in self.rawdata:
-#                 self.rawdata[city] = {}
-#             if year not in self.rawdata[city]:
-#                 self.rawdata[city][year] = {}
-#             if month not in self.rawdata[city][year]:
-#                 self.rawdata[city][year][month] = {}
-#             self.rawdata[city][year][month][day] = temperature
-#
-#         f.close()
-#
-#     def get_yearly_temp(self, city, year):
-#         """
-#         Get the daily temperatures for the given year and city.
-#
-#         Args:
-#             city: city name (str)
-#             year: the year to get the data for (int)
-#
-#         Returns:
-#             a numpy 1-d array of daily temperatures for the specified year and
-#             city
-#         """
-#         temperatures = []
-#         assert city in self.rawdata, "provided city is not available"
-#         assert year in self.rawdata[city], "provided year is not available"
-#         for month in range(1, 13):
-#             for day in range(1, 32):
-#                 if day in self.rawdata[city][year][month]:
-#                     temperatures.append(self.rawdata[city][year][month][day])
-#         return np.array(temperatures)
-#
-#     def get_daily_temp(self, city, month, day, year):
-#         """
-#         Get the daily temperature for the given city and time (year + date).
-#
-#         Args:
-#             city: city name (str)
-#             month: the month to get the data for (int, where January = 1,
-#                 December = 12)
-#             day: the day to get the data for (int, where 1st day of month = 1)
-#             year: the year to get the data for (int)
-#
-#         Returns:
-#             a float of the daily temperature for the specified time (year +
-#             date) and city
-#         """
-#         assert city in self.rawdata, "provided city is not available"
-#         assert year in self.rawdata[city], "provided year is not available"
-#         assert month in self.rawdata[city][year], "provided month is not available"
-#         assert day in self.rawdata[city][year][month], "provided day is not available"
-#         return self.rawdata[city][year][month][day]
-#
+INTERVAL_1 = list(range(1961, 2006))
+INTERVAL_2 = list(range(2006, 2016))
+
+"""
+Begin helper code
+"""
+
+
+class Climate(object):
+    """
+    The collection of temperature records loaded from given csv file
+    """
+
+    def __init__(self, filename):
+        """
+        Initialize a Climate instance, which stores the temperature records
+        loaded from a given csv file specified by filename.
+
+        Args:
+            filename: name of the csv file (str)
+        """
+        self.rawdata = {}
+
+        f = open(filename, 'r')
+        header = f.readline().strip().split(',')
+        for line in f:
+            items = line.strip().split(',')
+
+            date = re.match('(\d\d\d\d)(\d\d)(\d\d)', items[header.index('DATE')])
+            year = int(date.group(1))
+            month = int(date.group(2))
+            day = int(date.group(3))
+
+            city = items[header.index('CITY')]
+            temperature = float(items[header.index('TEMP')])
+            if city not in self.rawdata:
+                self.rawdata[city] = {}
+            if year not in self.rawdata[city]:
+                self.rawdata[city][year] = {}
+            if month not in self.rawdata[city][year]:
+                self.rawdata[city][year][month] = {}
+            self.rawdata[city][year][month][day] = temperature
+
+        f.close()
+
+    def get_yearly_temp(self, city, year):
+        """
+        Get the daily temperatures for the given year and city.
+
+        Args:
+            city: city name (str)
+            year: the year to get the data for (int)
+
+        Returns:
+            a numpy 1-d array of daily temperatures for the specified year and
+            city
+        """
+        temperatures = []
+        assert city in self.rawdata, "provided city is not available"
+        assert year in self.rawdata[city], "provided year is not available"
+        for month in range(1, 13):
+            for day in range(1, 32):
+                if day in self.rawdata[city][year][month]:
+                    temperatures.append(self.rawdata[city][year][month][day])
+        return np.array(temperatures)
+
+    def get_daily_temp(self, city, month, day, year):
+        """
+        Get the daily temperature for the given city and time (year + date).
+
+        Args:
+            city: city name (str)
+            month: the month to get the data for (int, where January = 1,
+                December = 12)
+            day: the day to get the data for (int, where 1st day of month = 1)
+            year: the year to get the data for (int)
+
+        Returns:
+            a float of the daily temperature for the specified time (year +
+            date) and city
+        """
+        assert city in self.rawdata, "provided city is not available"
+        assert year in self.rawdata[city], "provided year is not available"
+        assert month in self.rawdata[city][year], "provided month is not available"
+        assert day in self.rawdata[city][year][month], "provided day is not available"
+        return self.rawdata[city][year][month][day]
 
 
 """
@@ -136,9 +137,6 @@ def generate_models(x, y, degs):
         a list of numpy arrays, where each array is a 1-d array of coefficients
         that minimizes the squared error of the fitting polynomial
     """
-    # array1 = x, y, degs
-    # return [array([  3.51667102e-02,  -1.37199002e+02,   1.33764160e+05])]
-    print(x, y, degs)
     result = []
     for deg in degs:
         result.append(np.polyfit(x, y, deg))
@@ -157,10 +155,11 @@ def r_squared(y, estimated):
     """
     estimated_arr = np.array(estimated)
     measured = np.array(y)
-    estimated_error = ((estimated_arr - measured)**2).sum()
-    mean_of_measured = measured.sum()/len(measured)
-    variability = ((measured - mean_of_measured)**2).sum()
-    return 1 - estimated_error/variability
+    estimated_error = ((estimated_arr - measured) ** 2).sum()
+    mean_of_measured = measured.sum() / len(measured)
+    variability = ((measured - mean_of_measured) ** 2).sum()
+    return 1 - estimated_error / variability
+
 
 # Problem 3
 def evaluate_models_on_training(x, y, models):
@@ -203,23 +202,22 @@ def evaluate_models_on_training(x, y, models):
         plt.show()
 
 
+# Beginning of program
+raw_data = Climate('data.csv')
 
-# ### Begining of program
-# raw_data = Climate('data.csv')
-#
-# # Problem 3
-# y = []
-# x = INTERVAL_1
-# for year in INTERVAL_1:
-#     y.append(raw_data.get_daily_temp('BOSTON', 1, 10, year))
-# models = generate_models(x, y, [1])
-# evaluate_models_on_training(x, y, models)
-#
-#
-# # Problem 4: FILL IN MISSING CODE TO GENERATE y VALUES
-# x1 = INTERVAL_1
-# x2 = INTERVAL_2
-# y = []
-# # MISSING LINES
-# models = generate_models(x, y, [1])
-# evaluate_models_on_training(x, y, models)
+# Problem 3
+y = []
+x = INTERVAL_1
+for year in INTERVAL_1:
+    y.append(raw_data.get_daily_temp('BOSTON', 1, 10, year))
+models = generate_models(x, y, [1])
+evaluate_models_on_training(x, y, models)
+
+# Problem 4: FILL IN MISSING CODE TO GENERATE y VALUES
+x1 = INTERVAL_1
+x2 = INTERVAL_2
+y = []
+for year in INTERVAL_1:
+    y.append(np.mean(raw_data.get_yearly_temp('BOSTON', year)))
+models = generate_models(x1, y, [1])
+evaluate_models_on_training(x1, y, models)
